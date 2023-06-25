a96c5aed8b44c8c3f9395dd550e3c44fe0f1b216
Lukas P <99148241+Lukaspspsl@users.noreply.github.com>
2023-06-25 12:51:54 +0200

    Pspsl/company model (#4)
    * Založen model Company a zaregistrován v adminu
    
    * založen Company Model

--- webapp/users/models.py
+++ webapp/users/models.py
@@ -1,7 +1,20 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-   pass
+    pass
 
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    contact_person = models.CharField(max_length=255, blank=False, null=False)
+    telephone_number = models.IntegerField(default="no_number")
+    email = models.EmailField(max_length=60)
+    place = models.CharField(max_length=255, default="")
+
+    class Meta:
+        verbose_name_plural = ('Companies')
+
+    def __str__(self):
+        return self.name
