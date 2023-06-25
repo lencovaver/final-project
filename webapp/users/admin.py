5f22258bd26b6a735d4498477551e56fb0ce2880
Lukaspspsl <lukaspospisill@gmail.com>
2023-06-25 13:36:55 +0300

    Založen model Company a zaregistrován v adminu

--- webapp/users/admin.py
+++ webapp/users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Company
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "contact_person", "telephone_number", "email", "place"]
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Company, CompanyAdmin)
+
+
