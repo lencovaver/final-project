079224c95a0b52cfe091a81d7431e51a68eb9fa6
Lukaspspsl <lukaspospisill@gmail.com>
2023-06-28 20:26:12 +0200

    neznámá změna

--- webapp/jobapp/admin.py
+++ webapp/jobapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Place, PostJob,Positions
+from .models import Place, PostJob, Position
 
 admin.site.register(Place)
 admin.site.register(PostJob)
