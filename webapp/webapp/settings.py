6746daddd1c8115aa57e41645b300ee767b45cac
Lukas P <99148241+Lukaspspsl@users.noreply.github.com>
2023-07-02 11:37:06 +0200

    Pspsl/registration (#16)
    * přidána hrubá registrace, upravena struktura templates
    
    * registrace pro různé typy účtů
    
    * CRUD
    
    * nedelní update

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,8 +40,6 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
 
 ]
-
-
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
