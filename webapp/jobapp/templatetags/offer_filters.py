b51b6517514987553fb9aceecbf68aae9c4299cc
lencovaver <lencovaver@gmail.com>
2023-07-03 21:44:17 +0200

    pridani templatetags, offer_filters

---
+++ webapp/jobapp/templatetags/offer_filters.py
@@ -0,0 +1,10 @@
+from django import template
+from django.utils.html import format_html
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def format_offer(value):
+    formatted_value = f"<div class='job-offer'>{value}</div>"
+    return format_html(formatted_value)
