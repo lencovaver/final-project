29691fa6af49f19f94c4c49840ee2c5969f53b39
lencovaver <115870821+lencovaver@users.noreply.github.com>
2023-07-08 13:00:12 +0200

    Verca/sjednoceni (#20)
    * uprava UserAgent, UserPerson
    
    * uprava admin.py, pridani City a uprava CompanyAdmin
    
    * uprava vyberu jazyku
    
    * uprava formatovani
    
    * pridano multiselectfield
    
    * uprava DrivingLicence, lze pridat vice moznosti
    
    * uprava driving licence
    
    * pridani models.py
    
    * pridani models.py
    
    * umazani multiselectfield
    
    * pridani templatetags, offer_filters
    
    * úprava offer_filters.py a all-jobs.html
    
    * pridani DEBUG, static do url
    
    * pridani vyhledavaci kolonky do all-jobs.html
    
    * pridani JobSearchView, job_search.html + do urls.py, uprava offer_filters v templatetags
    
    * pridani uprav od Petra, hlavne templates
    
    * pridani job-detail a uprava job-search
    
    * uprava all-jobs.html
    
    * uprava views.py, job-search.html a all-jobs.html
    
    * uprava views.py, all-jobs.html, uz to vyhledava!
    
    * uprava EXPERIENCE_CHOICES
    
    * uprava models.py def clean
    
    * uprava Position a driving licence
    
    * uprava models.py
    
    ---------
    
    Co-authored-by: lencovaver <lencovaver@gmail.com>

--- webapp/jobapp/admin.py
+++ webapp/jobapp/admin.py
@@ -3,7 +3,11 @@ from .models import Place, PostJob, Position, Language, DrivingLicence
 
 
 class PostJobAdmin(admin.ModelAdmin):
-    list_display = ["positions", "place", "language", "accommodation", "salary", "diet"]
+    list_display = ["positions", "place", "get_languages", "accommodation", "salary", "diet"]
+
+    def get_languages(self, obj):
+        return ", ".join([str(language) for language in obj.language.all()])
+    get_languages.short_description = "Languages"
 
 
 admin.site.register(Place)
