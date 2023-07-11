80516f5d331d2d37d177188f736726d061aa5167
Lukas P <99148241+Lukaspspsl@users.noreply.github.com>
2023-07-11 19:05:49 +0200

    Delete webapp directory

--- webapp/jobapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Place, PostJob, Position
-
-
-admin.site.register(Place)
-admin.site.register(PostJob, PostJobAdmin)
-admin.site.register(Position)
-
