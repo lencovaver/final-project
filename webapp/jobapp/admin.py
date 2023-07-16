1b6ad7250a3768b2e887651f8b7f4a2e019dc65c
lencovaver <115870821+lencovaver@users.noreply.github.com>
2023-07-16 12:20:46 +0200

    Verca/vetev (#30)
    * vytvoreni PlaceSearchView a LanguageSerchView, pridani do urls.py a uprava v homepage.html
    
    * uprava LanguageSerchView a homepage.html
    
    * pridani archivace inzeratu
    
    ---------
    
    Co-authored-by: lencovaver <lencovaver@gmail.com>

--- webapp/jobapp/admin.py
+++ webapp/jobapp/admin.py
@@ -5,6 +5,15 @@ from .models import Place, PostJob, Position, Language, DrivingLicence
 
 
 class PostJobAdmin(admin.ModelAdmin):
+    list_display = ['positions', 'status']      # Zobrazovaná pole v seznamu
+    list_filter = ['status']                    # Filtr pro archivaci
+    actions = ['archive_selected_jobs']         # Přidání akce pro archivaci více inzerátů najednou
+
+    def archive_selected_jobs(self, request, queryset):
+        queryset.update(status='archived')
+        self.message_user(request, "Inzeráty byly úspěšně archivovány.")
+
+    archive_selected_jobs.short_description = "Archivovat vybrané inzeráty"
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
