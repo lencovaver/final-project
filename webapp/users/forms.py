2adaf564c730d1b4f8bf12fd394decf54fa8193d
Lukas P <99148241+Lukaspspsl@users.noreply.github.com>
2023-07-18 11:07:53 +0200

    upload a ukládání profilových obrázků a company logos (#33)

--- webapp/users/forms.py
+++ webapp/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from django import forms
+from django.core.exceptions import ValidationError
 
 from users.models import User
 
@@ -17,6 +18,11 @@ class RegistrationForm(UserCreationForm):
         }
 
     def clean_email(self):
+        """
+        Checks if email is already in use.
+        #FIX ERROR MESSAGE!
+        """
+
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Email is already in use.")
@@ -24,12 +30,16 @@ class RegistrationForm(UserCreationForm):
 
 
 class EditProfileForm(UserChangeForm):
+    # Form for editing user profile.
+
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     company_name = forms.CharField(max_length=200, required=False)
 
     class Meta:
         model = User
         fields = [
+            "profile_pic",
+            "company_logo",
             'first_name',
             'last_name',
             "address",
@@ -40,3 +50,18 @@ class EditProfileForm(UserChangeForm):
         ]
         exclude = ['password', "company"]
 
+    def clean_profile_pic(self):
+        # Checks if profile picture is too large.
+
+        image = self.cleaned_data.get('profile_pic', False)
+        if image and image.size > 1 * 1024 * 1024:
+            raise ValidationError("Image file too large - must be no more than 1MB")
+        return image
+
+    def clean_company_logo(self):
+        # Checks if company logo is too large.
+
+        image = self.cleaned_data.get('company_logo', False)
+        if image and image.size > 1 * 1024 * 1024:
+            raise ValidationError("Image file too large - must be no more than 1MB")
+        return image
