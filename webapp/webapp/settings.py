0fa2c35bfc3ff4b1c62cda9b0e77daa4ce75c942
lencovaver <115870821+lencovaver@users.noreply.github.com>
2023-07-30 13:01:07 +0200

    Update settings.py

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -146,5 +146,3 @@ MEDIA_URL = '/media/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
