33449a1f1ae9d8008d0caae64ba2f6dc3344d01f
Lukas P <99148241+Lukaspspsl@users.noreply.github.com>
2023-08-03 18:13:53 +0200

    fix job detailu (#55)

--- webapp/jobapp/views.py
+++ webapp/jobapp/views.py
@@ -3,8 +3,19 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from .forms import PostJobForm
 from .models import PostJob, Place, Language, PositionCategory, Position
 
@@ -21,7 +32,7 @@ class HomepageView(TemplateView):
         places = Place.objects.all()
         context["places"] = places
 
-        context['jobs'] = PostJob.objects.all()
+        context["jobs"] = PostJob.objects.all()
         return context
 
 
@@ -31,18 +42,18 @@ class AllJobsView(ListView):
     context_object_name = "jobs"
 
     def get_queryset(self):
-        return PostJob.objects.filter(status='active')
+        return PostJob.objects.filter(status="active")
 
 
 class JobDetailView(LoginRequiredMixin, DetailView):
     model = PostJob
     template_name = "job-detail.html"
-    context_object_name = 'job'
+    context_object_name = "job"
 
     def get_object(self):
         job_id = self.kwargs.get("job_id")
         job = get_object_or_404(PostJob, id=job_id)
-        if job.status != 'active':
+        if job.status != "active":
             raise Http404
         return job
 
@@ -50,21 +61,21 @@ class JobDetailView(LoginRequiredMixin, DetailView):
 class JobCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = PostJob
     form_class = PostJobForm
-    template_name = 'create.html'
+    template_name = "create.html"
 
     def test_func(self):
         return self.request.user.is_useragent or self.request.user.is_superuser
 
     def handle_no_permission(self):
         messages.error(self.request, "You don't have permission.")
-        return redirect('homepage')
+        return redirect("homepage")
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         instance = form.save(commit=False)
         instance.save()
         form.save_m2m()
-        return redirect('job-success')
+        return redirect("job-success")
 
     def form_invalid(self, form):
         print(form.errors)
@@ -74,17 +85,20 @@ class JobCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 class JobUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = PostJob
     form_class = PostJobForm
-    template_name = 'update.html'
-    success_url = reverse_lazy('job-success')
+    template_name = "update.html"
+    success_url = reverse_lazy("job-success")
 
     def test_func(self):
-        return self.request.user == self.get_object().author or self.request.user.is_superuser
+        return (
+            self.request.user == self.get_object().author
+            or self.request.user.is_superuser
+        )
 
-    permission_required = 'jobapp.add_postjob'
+    permission_required = "jobapp.add_postjob"
 
     def handle_no_permission(self):
         messages.error(self.request, "No permission.")
-        return redirect('homepage')
+        return redirect("homepage")
 
     def form_valid(self, form):
         print(form.cleaned_data)
@@ -93,17 +107,20 @@ class JobUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class JobDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = PostJob
-    template_name = 'delete.html'
-    success_url = reverse_lazy('job-success')
+    template_name = "delete.html"
+    success_url = reverse_lazy("job-success")
 
     def test_func(self):
-        return self.request.user == self.get_object().author or self.request.user.is_superuser
+        return (
+            self.request.user == self.get_object().author
+            or self.request.user.is_superuser
+        )
 
-    permission_required = 'jobapp.add_postjob'
+    permission_required = "jobapp.add_postjob"
 
     def handle_no_permission(self):
         messages.error(self.request, "No permission.")
-        return redirect('homepage')
+        return redirect("homepage")
 
     def form_valid(self, form):
         response = super().delete(request)
@@ -112,7 +129,7 @@ class JobDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 class JobSuccessView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'job-success.html')
+        return render(request, "job-success.html")
 
 
 class JobSearchView(ListView):
@@ -121,7 +138,7 @@ class JobSearchView(ListView):
     context_object_name = "jobs"
 
     def get_queryset(self):
-        queryset = PostJob.objects.filter(status='active')
+        queryset = PostJob.objects.filter(status="active")
         query = self.request.GET.get("title_contains")
         if query:
             queryset = queryset.filter(position__name_position__icontains=query)
@@ -139,8 +156,8 @@ class JobSearchView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Přidejme dostupné kantony a jazyky do kontextu
-        context['places'] = Place.objects.all()
-        context['languages'] = Language.objects.all()
+        context["places"] = Place.objects.all()
+        context["languages"] = Language.objects.all()
         return context
 
 
@@ -152,13 +169,13 @@ class PlaceSearchView(ListView):
     def get_queryset(self):
         place_id = self.request.GET.get("place_id")
         if place_id:
-            return PostJob.objects.filter(place__pk=place_id, status='active')
+            return PostJob.objects.filter(place__pk=place_id, status="active")
         else:
-            return PostJob.objects.filter(status='active')
+            return PostJob.objects.filter(status="active")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['postjobs'] = PostJob.objects.filter(status='active')
+        context["postjobs"] = PostJob.objects.filter(status="active")
         return context
 
 
@@ -170,9 +187,9 @@ class LanguageSearchView(ListView):
     def get_queryset(self):
         state_name = self.request.GET.get("state")
         if state_name:
-            return PostJob.objects.filter(language__state=state_name, status='active')
+            return PostJob.objects.filter(language__state=state_name, status="active")
         else:
-            return PostJob.objects.filter(status='active')
+            return PostJob.objects.filter(status="active")
 
 
 class JobArchiveView(ListView):
@@ -192,4 +209,6 @@ def archive_job(request, pk):
     inzerat_admin = PostJob.objects.get(pk=pk)
     inzerat_admin.status = "archived" if inzerat.archived else "active"
     inzerat_admin.save()
-    return JsonResponse({"message": "Inzerát je archivován.", "archived": inzerat.archived})
+    return JsonResponse(
+        {"message": "Inzerát je archivován.", "archived": inzerat.archived}
+    )
