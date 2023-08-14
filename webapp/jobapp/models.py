c8433aa7e239ac47911bc6a05455daa4829e1c69
Lukas P <lukaspospisill@gmail.com>
2023-08-14 21:39:56 +0200

    opraveny jazyky v job-detail

--- webapp/jobapp/models.py
+++ webapp/jobapp/models.py
@@ -5,8 +5,8 @@ class PositionCategory(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ['name']
-        verbose_name_plural = 'Position Categories'
+        ordering = ["name"]
+        verbose_name_plural = "Position Categories"
 
     def __str__(self):
         return self.name
@@ -14,7 +14,9 @@ class PositionCategory(models.Model):
 
 class Position(models.Model):
     name_position = models.CharField(max_length=100)
-    category = models.ForeignKey(PositionCategory, on_delete=models.CASCADE, blank=False)
+    category = models.ForeignKey(
+        PositionCategory, on_delete=models.CASCADE, blank=False
+    )
 
     def __str__(self):
         return self.name_position
@@ -25,7 +27,7 @@ class Place(models.Model):
     shortname = models.CharField(max_length=3)
 
     class Meta:
-        ordering = ['name_place']
+        ordering = ["name_place"]
 
     def __str__(self):
         return self.name_place
@@ -33,61 +35,39 @@ class Place(models.Model):
 
 class Language(models.Model):
     STATE_CHOICES = [
-        ('ENG', '🇬🇧 angličtina'),
-        ('CHF', '🇨🇭 švýcarská němčina'),
-        ('DEU', '🇩🇪 němčina'),
-        ('FRA', '🇫🇷 francouzština'),
-        ('ITA', '🇮🇹 italština'),
+        ("ENG", "🇬🇧 angličtina"),
+        ("CHF", "🇨🇭 švýcarská němčina"),
+        ("DEU", "🇩🇪 němčina"),
+        ("FRA", "🇫🇷 francouzština"),
+        ("ITA", "🇮🇹 italština"),
     ]
     LEVEL_CHOICES = [
-        ('1', 'A1 - začátečník'),
-        ('2', 'A2 - pokročilý začátečník'),
-        ('3', 'B1 - mírně pokročilý'),
-        ('4', 'B2 - středně pokročilý'),
-        ('5', 'C1 - velmi pokročilý'),
-        ('6', 'C2 - expert'),
+        ("1", "A1 - začátečník"),
+        ("2", "A2 - pokročilý začátečník"),
+        ("3", "B1 - mírně pokročilý"),
+        ("4", "B2 - středně pokročilý"),
+        ("5", "C1 - velmi pokročilý"),
+        ("6", "C2 - expert"),
     ]
     state = models.CharField(max_length=3, choices=STATE_CHOICES, default="ENG")
     level = models.CharField(max_length=2, choices=LEVEL_CHOICES, default="1")
 
-    def get_level_name(self):
-        for level in self.LEVEL_CHOICES:
-            if level[0] == self.level:
-                return level[1].split(" - ")[0]
-
-    def get_full_name(self):
-        for state in self.STATE_CHOICES:
+    def get_language_display(self):
+        for state in Language.STATE_CHOICES:
             if state[0] == self.state:
-                return f"{state[1]} - {self.get_level_name()}"
+                return state[1]
+
+    def get_level_display(self):
+        for level in Language.LEVEL_CHOICES:
+            if level[0] == self.level:
+                return level[1]
 
     def __str__(self):
-        return f"{self.state} - {self.get_level_name()}"
+        return f"{self.get_language_display()} - {self.get_level_display()}"
 
 
 class DrivingLicence(models.Model):
     licence_category = models.CharField(max_length=6, default="")
-    #short_name = models.CharField(max_length=3, default="")
-
-    #CATEGORY_CHOICES = [
-    #    ('', '---------'),
-    #    ('AM', 'AM 🛵'),
-    #    ('A1', 'A1'),
-    #    ('A2', 'A2'),
-    #    ('A', 'A 🏍️'),
-    #    ('B1', 'B1 🚚'),
-    #    ('B', 'B 🚗'),
-    #    ('C1', 'C1 🚛'),
-    #    ('C', 'C'),
-    #    ('D1', 'D1 🚐'),
-    #    ('D', 'D 🚌'),
-    #    ('BE', 'BE '),
-    #    ('C1E', 'C1E'),
-    #    ('CE', 'CE'),
-    #    ('D1E', 'D1E'),
-    #    ('DE', 'DE'),
-    #    ('T', 'T 🚜')
-    #]
-#field = models.CharField(max_length=6, choices=CATEGORY_CHOICES, blank=True)
 
     def __str__(self):
         return self.licence_category
@@ -95,14 +75,14 @@ class DrivingLicence(models.Model):
 
 class PostJob(models.Model):
     EXPERIENCE_CHOICES = [
-        ('1-3 roky', '1-3'),
-        ('4-6 let', '4-6'),
-        ('6-9 let', '6-9'),
-        ('10 a více let', '10 a více'),
+        ("1-3 roky", "1-3"),
+        ("4-6 let", "4-6"),
+        ("6-9 let", "6-9"),
+        ("10 a více let", "10 a více"),
     ]
     ACCOMMODATION_CHOICES = [
-        ('vlastní ubytování', 'mám zajištěné'),
-        ('zajištěné ubytování', 'chci zajistit'),
+        ("vlastní ubytování", "mám zajištěné"),
+        ("zajištěné ubytování", "chci zajistit"),
     ]
     STATUS_CHOICES = [
         ("active", "Aktivní"),
@@ -114,22 +94,29 @@ class PostJob(models.Model):
         ("brigáda", "Brigáda"),
     ]
     id = models.AutoField(primary_key=True)
-    author = models.ForeignKey("users.User", on_delete=models.CASCADE, default='')
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE, default="")
     archived = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    position = models.ForeignKey(Position, related_name="position", on_delete=models.CASCADE)
+    position = models.ForeignKey(
+        Position, related_name="position", on_delete=models.CASCADE
+    )
     driving_licence = models.ManyToManyField(DrivingLicence, blank=True)
-    experience = models.CharField(max_length=100, choices=EXPERIENCE_CHOICES, default='1-3')
-    place = models.ForeignKey(Place, related_name="postjobs", on_delete=models.CASCADE, null=True)
-    language = models.ManyToManyField(Language, related_name="language")
-    accommodation = models.CharField(max_length=30, choices=ACCOMMODATION_CHOICES, default='')
+    experience = models.CharField(
+        max_length=100, choices=EXPERIENCE_CHOICES, default="1-3"
+    )
+    place = models.ForeignKey(
+        Place, related_name="postjobs", on_delete=models.CASCADE, null=True
+    )
+    language = models.ManyToManyField(Language, related_name="language", blank=True)
+    accommodation = models.CharField(
+        max_length=30, choices=ACCOMMODATION_CHOICES, default=""
+    )
     info_position = models.TextField()
     work_type = models.CharField(max_length=10, choices=WORK_TYPE_CHOICES, default="")
     salary = models.IntegerField(default=30)
     diet = models.IntegerField(default=0)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="active")
     start_date = models.DateField()
-
 
     def __str__(self):
         return self.position.name_position
